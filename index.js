e77a9ea182eab6231a531648fe068e2b01ea7d40
Pawel Buderaski <pbuderaski@gmail.com>
2023-05-16 23:32:01 +0100

    feat: updated the code

--- index.js
+++ index.js
@@ -1,65 +1,71 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const token = core.getInput("github-token");
-const octokit = github.getOctokit(token);
-
-const pr = github.context.payload.pull_request;
-const labels = pr.labels.map((label) => label.name);
-let targetBranches = [];
-
-console.log("pr", pr)
-
-// Extract version number from labels and construct target branches
-labels.forEach((label) => {
-    const versionMatch = label.match(/^v(\d+)$/);
-    if (versionMatch) {
-        const versionNumber = versionMatch[1];
-        const targetBranch = `release/version-${versionNumber}`;
-        targetBranches.push(targetBranch);
-    }
-});
-
-console.log("labels", labels)
-// Cherry-pick and create pull requests for each target branch
-targetBranches.forEach(async (branch) => {
+async function run() {
     try {
-        console.log(`Creating cherry-pick commit to ${branch}`);
-        console.log({            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            message: `Cherry-pick ${pr.number} to ${branch}`,
-            parents: [pr.merge_commit_sha],
-            tree: pr.head.sha,})
-        const { data: commit } = await octokit.rest.git.createCommit({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            message: `Cherry-pick ${pr.number} to ${branch}`,
-            parents: [pr.merge_commit_sha],
-            tree: pr.head.sha,
+        const token = core.getInput("github-token");
+        const octokit = github.getOctokit(token);
+
+        const pr = github.context.payload.pull_request;
+        const labels = pr.labels.map((label) => label.name);
+        let targetBranches = [];
+
+        // Extract version number from labels and construct target branches
+        labels.forEach((label) => {
+            const versionMatch = label.match(/^v(\d+)$/);
+            if (versionMatch) {
+                const versionNumber = versionMatch[1];
+                const targetBranch = `release/version-${versionNumber}`;
+                targetBranches.push(targetBranch);
+            }
         });
 
-        console.log(`Creating pull request to ${branch}`);
-        const originalPR = await octokit.rest.pulls.get({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            pull_number: pr.number,
-        });
+        for (const branch of targetBranches) {
+            try {
+                console.log(`Creating new branch ${branch}-cherry-pick`);
+                const newBranchRef = await octokit.rest.git.createRef({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    ref: `refs/heads/${branch}-cherry-pick`,
+                    sha: pr.head.sha,
+                });
 
-        await octokit.rest.pulls.create({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            title: originalPR.data.title,
-            head: commit.sha,
-            base: branch,
-        });
+                console.log(`Cherry-picking commit onto ${branch}-cherry-pick`);
+                await octokit.rest.git.createCommit({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    message: `Cherry-pick ${pr.number} to ${branch}`,
+                    parents: [newBranchRef.data.object.sha],
+                    tree: pr.head.sha,
+                });
 
-        console.log(
-            `Cherry-pick and pull request creation for ${branch} completed successfully`
-        );
+                console.log(`Creating pull request to ${branch}`);
+                const originalPR = await octokit.rest.pulls.get({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    pull_number: pr.number,
+                });
+
+                await octokit.rest.pulls.create({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    title: originalPR.data.title,
+                    head: `${github.context.repo.owner}:${branch}-cherry-pick`,
+                    base: branch,
+                });
+
+                console.log(
+                    `Cherry-pick and pull request creation for ${branch} completed successfully`
+                );
+            } catch (error) {
+                console.error(
+                    `Error occurred while cherry-picking and creating pull request for ${branch}`
+                );
+                console.error(error);
+            }
+        }
     } catch (error) {
-        console.error(
-            `Error occurred while cherry-picking and creating pull request for ${branch}`
-        );
+        console.error("An error occurred:");
         console.error(error);
     }
-});
+}

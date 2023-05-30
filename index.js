8c147501bc0a38cb455a52be93dc3ac1d25c2ff9
Pawel Buderaski <pbuderaski@gmail.com>
2023-05-30 13:52:27 +0100

    feat: checkout before pulling

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ async function run() {
       try {
         const uniqueBranchName = `${branch}-cherry-pick-${Date.now()}`;
 
-
-        await exec.exec('git', ['pull', 'origin', branch]); // Fetch the target branch from the remote repository
+        await exec.exec('git', ['fetch', 'origin',  branch]);
         await exec.exec('git', ['checkout', branch]);
+        await exec.exec('git', ['pull', 'origin', branch]); // Fetch the target branch from the remote repository
         await exec.exec('git', ['checkout', '-b', uniqueBranchName]);
         await exec.exec('git', ['cherry-pick', pr.merge_commit_sha]);
 
